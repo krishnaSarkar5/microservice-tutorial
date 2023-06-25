27fa2d61bd9ee490885572f86e8151bd78a20ba8
krishnasarkar5 <krishnasarkar95@gmail.com>
2023-06-25 23:20:10 +0530

    added sensitive data in vault

--- order-service/src/main/java/com/ecommerce/orderservice/controller/TestController.java
+++ order-service/src/main/java/com/ecommerce/orderservice/controller/TestController.java
@@ -1,16 +1,21 @@
 package com.ecommerce.orderservice.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@RefreshScope
 public class TestController {
 
+    @Value("${test}")
+    private String test;
     @GetMapping("/test")
     public String test(){
-        return "Hello";
+        return test;
     }
 }
